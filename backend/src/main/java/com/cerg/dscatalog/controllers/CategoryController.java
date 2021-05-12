15343bec887f505eda9c5c4512668bd2fb323c51
Felipe Kinupes Gouvêa <felipekinupesg@hotmail.com>
2021-05-12 00:22:27 -0300

    CategoryController

---
+++ backend/src/main/java/com/cerg/dscatalog/controllers/CategoryController.java
@@ -0,0 +1,25 @@
+package com.cerg.dscatalog.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cerg.dscatalog.entities.Category;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll(){
+		List<Category> list = new ArrayList<>();
+		list.add(new Category(1L, "Eletronicos"));
+		list.add(new Category(2L, "Cama, mesa e banho"));
+		return ResponseEntity.ok().body(list);
+	}
+
+}
---
+++ backend/src/main/java/com/cerg/dscatalog/services/CategoryService.java
@@ -0,0 +1,8 @@
+package com.cerg.dscatalog.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+}
