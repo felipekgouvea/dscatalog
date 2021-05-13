bab73a25958c9a7f0a15fc6e8835a6f92dda4546
Felipe Kinupes Gouvêa <felipekinupesg@hotmail.com>
2021-05-13 10:58:50 -0300

    Auditing data

--- backend/src/main/java/com/cerg/dscatalog/entities/Category.java
+++ backend/src/main/java/com/cerg/dscatalog/entities/Category.java
@@ -1,11 +1,15 @@
 package com.cerg.dscatalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -25,5 +29,20 @@ public class Category implements Serializable{
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
 
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updateAt;
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updateAt = Instant.now();
+	}
 }
