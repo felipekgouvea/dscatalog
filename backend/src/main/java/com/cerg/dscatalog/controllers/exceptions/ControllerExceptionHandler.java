2e3201d7341870686584bcdeae76471c9aa604b1
Felipe Kinupes Gouvêa <felipekinupesg@hotmail.com>
2021-06-03 15:31:45 -0300

    Implementing OAuth authorization server

--- backend/src/main/java/com/cerg/dscatalog/controllers/exceptions/ControllerExceptionHandler.java
+++ backend/src/main/java/com/cerg/dscatalog/controllers/exceptions/ControllerExceptionHandler.java
@@ -46,7 +46,7 @@ public class ControllerExceptionHandler {
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ValidationError> database (MethodArgumentNotValidException e, HttpServletRequest request){
+	public ResponseEntity<ValidationError> validation (MethodArgumentNotValidException e, HttpServletRequest request){
 		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
 		ValidationError err = new ValidationError();
 		
--- backend/src/main/java/com/cerg/dscatalog/controllers/exceptions/StandardError.java
+++ backend/src/main/java/com/cerg/dscatalog/controllers/exceptions/StandardError.java
@@ -3,11 +3,6 @@ package com.cerg.dscatalog.controllers.exceptions;
 import java.io.Serializable;
 import java.time.Instant;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
 public class StandardError implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
@@ -16,4 +11,56 @@ public class StandardError implements Serializable {
 	private String error;
 	private String message;
 	private String path;
+	
+	public StandardError() {
+	}
+	
+	public StandardError(Instant timestamp, Integer status, String error, String message, String path) {
+		super();
+		this.timestamp = timestamp;
+		this.status = status;
+		this.error = error;
+		this.message = message;
+		this.path = path;
+	}
+
+	public Instant getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Instant timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
 }
