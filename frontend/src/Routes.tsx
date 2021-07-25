6b11db5d0cfcf3f2513db0b17090e1b98691c8ab
Felipe Kinupes Gouvêa <felipekinupesg@hotmail.com>
2021-07-25 13:36:02 -0300

    Redirection to login when 401 or 403

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,13 +1,14 @@
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { Router, Switch, Route, Redirect } from 'react-router-dom';
 import Home from './pages/Home';
 import Catalog from './pages/Catalog';
 import NavBar from './components/NavBar';
 import ProductDetails from './pages/ProductDetails';
 import Admin from './pages/Admin';
 import Auth from './pages/Admin/Auth';
+import history from './util/history';
 
 const Routes = () => (
-  <BrowserRouter>
+  <Router history={history}>
     <NavBar />
     <Switch>
       <Route path="/" exact>
@@ -28,7 +29,7 @@ const Routes = () => (
         <Admin />
       </Route>
     </Switch>
-  </BrowserRouter>
+  </Router>
 );
 
 export default Routes;
