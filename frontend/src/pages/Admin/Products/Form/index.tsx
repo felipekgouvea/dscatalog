1b07b2d0f9911d417e01143695be5d1377061d3c
Felipe Kinupes Gouvêa <felipekinupesg@hotmail.com>
2021-08-06 09:28:26 -0300

    Product crud form HTML

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -1,10 +1,31 @@
+import './styles.css';
+
 const Form = () => {
-    return (
-      <div>
-        <h1>FORMULÁRIO</h1>
+  return (
+    <div className="product-crud-container">
+      <div className="base-card product-crud-form-card">
+        <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
+
+        <form action="">
+          <div className="row">
+            <div className="col-lg-6">
+              <input type="text" className="form-control base-input"/>
+              <input type="text" className="form-control base-input"/>
+              <input type="text" className="form-control base-input"/>
+            </div>
+            <div className="col-lg-6">
+              <textarea name="" rows={10} className="form-control base-input" ></textarea>
+            </div>
+          </div>
+
+          <div>
+            <button className="btn btn-outline-danger">CANCELAR</button>
+            <button className="btn btn-primary">SALVAR</button>
+          </div>
+        </form>
       </div>
-    );
-  };
-  
-  export default Form;
-  
+    </div>
+  );
+};
+
+export default Form;
